2e4c943feca601fd71dc941dcec82cf7c35eb65f
Yanira <yanirafr@gmail.com>
2020-11-09 02:30:11 +0100

    add conditionals

--- src/components/App.js
+++ src/components/App.js
@@ -14,17 +14,51 @@ class App extends Component {
     // console.log(this.translation);
     // console.log(userText);
     this.forceUpdate();
-    var saveWords = userText.split(" ");
-    // console.log(saveWords);
+    var savedWords = userText.split(" ");
+    // console.log(savedWords);
     var changeWords = [];
-    for (let word in saveWords) {
-      // console.log(saveWords[word]);
-      var wordLength = saveWords[word].length - 1;
+    for (let word in savedWords) {
+      // console.log(savedWords[word]);
+      var wordLength = savedWords[word].length - 1;
       // console.log(wordLength);
-      var lastLetter = saveWords[word].substr(wordLength);
-      console.log(lastLetter);
-      changeWords.push(saveWords[word].substr(0, wordLength).concat("e "));
-      // console.log(changeWords[word]);
+      var lastLetter = savedWords[word].substr(wordLength);
+      // console.log(lastLetter);
+      var lastTwoLetters = savedWords[word].substr(wordLength - 1, wordLength);
+      // console.log(lastTwoLetters)
+      var lastThrLetters = savedWords[word].substr(wordLength - 2, wordLength);
+
+      // COS / CAS
+      if (lastThrLetters === "cos" || lastThrLetters === "cas") {
+        changeWords.push(
+          savedWords[word].substr(0, wordLength - 2).concat("ques ")
+        );
+        console.log("en el cos/cas", savedWords[word]);
+      }
+      // CO / CA
+      else if (lastTwoLetters === "co" || lastTwoLetters === "ca") {
+        changeWords.push(
+          savedWords[word].substr(0, wordLength - 1).concat("que ")
+        );
+        console.log("en el co/ca", savedWords[word]);
+      }
+      // OS / AS
+      else if (lastTwoLetters === "os" || lastTwoLetters === "as") {
+        changeWords.push(
+          savedWords[word].substr(0, wordLength - 1).concat("es ")
+        );
+        console.log("en el os/as", savedWords[word]);
+      }
+      // O / A
+      else if (lastLetter === "o" || lastLetter === "a") {
+        changeWords.push(savedWords[word].substr(0, wordLength).concat("e "));
+        console.log("en el else o/a", savedWords[word]);
+      }
+      // Else
+      else {
+        changeWords.push(savedWords[word]);
+        console.log("en el else", savedWords[word]);
+      }
+      console.log(changeWords[word]);
     }
   }
 
